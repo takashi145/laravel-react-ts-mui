1d9e4251ebe6113841d894d39ed6a42cfaab8fde
takashi145 <t.shuhei0203@gmail.com>
2025-09-27 00:11:51 +0900

    Homeコンポーネントでlaravel apiを呼び出し、レスポンス表示

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -1,11 +1,22 @@
 import { Button } from '@mui/material';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [message, setMessage] = useState<string>('');
+
+  useEffect(() => {
+    axios.get('/api')
+      .then(response => {
+        setMessage(response.data.message);
+      });
+  }, []);
+
   return (
-  <>
-    <h1>Home</h1>
-    <Button variant="contained" href="/about">About</Button>
-  </>
+    <>
+      <h1>Home</h1>
+      <p>{message}</p>
+      <Button variant="contained" href="/about">About</Button>
+    </>
   );
 }
