ba46cd8b7e33e24ea51dcfd763b80928c9cb6de1
takashi145 <t.shuhei0203@gmail.com>
2025-09-27 00:11:51 +0900

    Homeコンポーネントでlaravel apiを呼び出し、レスポンス表示

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('', function () {
+	return response()->json(['message' => 'Hello, API!']);
+});
