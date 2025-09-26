3c5f53a1c679ae4df0283d10b78083e9ff6b1f45
takashi145 <t.shuhei0203@gmail.com>
2025-09-26 23:30:23 +0900

    MUIを導入

--- resources/js/components/App.tsx
+++ resources/js/components/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
 export default function App() {
-  return <h1>Hello, React</h1>;
+  return (
+	<>
+		<h1>Hello, React</h1>
+		<Button variant="contained">MUIボタン</Button>
+	</>
+  );
 }
