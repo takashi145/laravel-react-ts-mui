49400b4f5531898285fbefd2fb11ea20fed3041c
takashi145 <t.shuhei0203@gmail.com>
2025-09-26 23:34:20 +0900

    tailwindcssを削除

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -10,6 +9,5 @@ export default defineConfig({
             refresh: true,
         }),
 		react(),
-        tailwindcss(),
     ],
 });
